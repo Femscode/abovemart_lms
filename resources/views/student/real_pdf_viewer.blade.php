496f2c3025ae7c39a6780a84fd72caec68844bb9
Femscode <fasanyafemi@gmail.com>
2023-11-11 21:45:16 +0100

    poi

--- resources/views/student/real_pdf_viewer.blade.php
+++ resources/views/student/real_pdf_viewer.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 <div class="col-xl-9">
     <a onclick='window.history.back()' class='btn btn-info'>Back To Library </a>
-    {{ $pdfPath }} {{ $path }}
-    <iframe src="/abovemart_lms/{{ $pdfPath }}" width="100%" height="600"></iframe>
-    <iframe src="/abovemart_lms/public/{{ $pdfPath }}" width="100%" height="600"></iframe>
+   
+    <iframe src="/abv_lms/{{ $pdfPath }}" width="100%" height="600"></iframe>
+    <iframe src="/abv_lms/public/{{ $pdfPath }}" width="100%" height="600"></iframe>
     <iframe src="{{ asset('abovemart_lms/'.$pdfPath) }}" width="100%" height="600"></iframe>
     {{-- <iframe src="{{ $pdfPath }}" width="100%" height="600"></iframe> --}}
 </div>
