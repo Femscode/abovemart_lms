1359e3e1315bcf826d76d30c4899501ec0e8762e
Femscode <fasanyafemi@gmail.com>
2023-11-11 20:57:02 +0100

    path3

--- resources/views/student/real_pdf_viewer.blade.php
+++ resources/views/student/real_pdf_viewer.blade.php
@@ -4,7 +4,8 @@
 
 @section('content')
 <div class="col-xl-9">
-    <a onclick='window.history.back()' class='btn btn-info'>Back To Library {{ $pdfPath }}</a>
+    <a onclick='window.history.back()' class='btn btn-info'>Back To Library </a>
+    {{ $pdfPath }}
     <iframe src="{{ $pdfPath }}" width="100%" height="600"></iframe>
 </div>
 @endsection
