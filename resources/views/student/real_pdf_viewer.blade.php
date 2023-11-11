290c52011e4c9075a5fa3c61327cbbcfe95f1639
Femscode <fasanyafemi@gmail.com>
2023-11-11 21:19:52 +0100

    path4

--- resources/views/student/real_pdf_viewer.blade.php
+++ resources/views/student/real_pdf_viewer.blade.php
@@ -6,7 +6,8 @@
 <div class="col-xl-9">
     <a onclick='window.history.back()' class='btn btn-info'>Back To Library </a>
     {{ $pdfPath }} {{ $path }}
-    <iframe src="{{ $pdfPath }}" width="100%" height="600"></iframe>
+    <iframe src="/abovemart_lms/{{ $pdfPath }}" width="100%" height="600"></iframe>
+    {{-- <iframe src="{{ $pdfPath }}" width="100%" height="600"></iframe> --}}
 </div>
 @endsection
 @section('script')
