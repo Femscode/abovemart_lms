798d1572c9026577529f24b4d6ab2a038a008d04
Femscode <fasanyafemi@gmail.com>
2023-11-12 14:37:38 +0100

    publ_path

--- resources/views/student/all_ebooks.blade.php
+++ resources/views/student/all_ebooks.blade.php
@@ -79,7 +79,8 @@
 				<div class="row g-0">
 					<div class="col-md-5">
 						@if($ebook->image !== null)
-						<img src="{{ asset('ebook_images/'.$ebook->image) }}" class="rounded-2" alt="Card image">
+						<img src="{{ public_path('ebook_images/'.$ebook->image) }}" class="rounded-2" alt="Card image">
+
 						@else
 						<img src="{{ asset('ebook_images/pdf.png') }}" class="rounded-2" alt="Card image">
 						@endif
